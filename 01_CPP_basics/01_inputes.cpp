cd609a612186efc85e2931e34d3f0bd26b4d2dd4
Prithvi Katkar <prithvirajkatkar17@gmail.com>
2026-03-03 12:56:56 +0530

    started with baiscs

---
+++ 01_CPP_basics/01_inputes.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int age;
+    cout<<"Enter your age: ";
+    cin>>age;
+
+    cout<<"Your age is: "<<age<<endl;
+
+    int a;
+    int b;
+    // input a
+    cout<<"enter a:";
+    cin>>a;
+
+    // input b 
+
+    cout<<"enter b:";
+    cin>>b;
+
+    // calculate sum
+
+    int sum =a+b;
+    cout<<"sum ="<<sum<<endl;
+
+    return 0;
+
+}
