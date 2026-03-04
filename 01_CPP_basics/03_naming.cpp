03a5e4baaee13715a8f86e9055e5efed700a93fd
Prithvi Katkar <prithvirajkatkar17@gmail.com>
2026-03-04 09:55:34 +0530

    added datatypes

---
+++ 01_CPP_basics/03_naming.cpp
@@ -0,0 +1,15 @@
+#include  <iostream>
+using namespace std;
+
+int main (){
+    int age = 25;
+    int marks = 90;
+    char grade = 'A';
+    bool isAdult = true;
+    float height = 5.9f;
+    cout << "Age: " << age << endl;
+    cout << "Marks: " << marks << endl;
+    cout << "Grade: " << grade << endl;
+    cout << "Is Adult: " << isAdult << endl;
+    cout << "Height: " << height << endl;
+}
