7508284181ec16c0ac9f0e4935874434583df1db
Prithvi Katkar <prithvirajkatkar17@gmail.com>
2026-03-05 21:41:43 +0530

    added arithmatic operator

---
+++ 02_operators/01_Arithmatic.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int a = 5;
+int b = 10;
+
+int main(){
+    cout<<"+ "<<(a+b)<<endl;
+    cout <<"-"<<(a-b)<<endl;
+    cout<<"* "<<(a*b)<<endl;
+    cout<<"/ "<<(a/b)<<endl;
+    cout<<"% "<<(b%a)<<endl;
+}
