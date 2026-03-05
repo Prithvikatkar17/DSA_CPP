3eaae6b215a5f8b5d982511c5210ae63211cd0b3
Prithvi Katkar <prithvirajkatkar17@gmail.com>
2026-03-05 21:47:12 +0530

    added assignment operator

---
+++ 02_operators/02_Assignment.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    int a = 10;
+    int b = 5;
+    a += 5; // This is equivalent to a = a + 5;
+    cout << "a = " << a << endl;
+    /*
+    -=  // This is equivalent to a = a - b; 
+    +=  // This is equivalent to a = a + b;
+    *=  // This is equivalent to a = a * b; 
+    /=  // This is equivalent to a = a / b; 
+    */  
+    
+}
