e6b9b16f6cbc2995c2eca7c7c19519ab5fcf2e16
Prithvi Katkar <prithvirajkatkar17@gmail.com>
2026-03-06 20:33:22 +0530

    added Explicit operator

---
+++ 02_operators/03_Explicit.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    float PI = 3.14;
+    cout << (int)(PI) << endl; // 3
+    cout << ((float)10 / 3) << endl; // 3.333
+    cout << (char)('A' + 3) << endl; // 'D'
+}
