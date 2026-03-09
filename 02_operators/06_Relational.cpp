c6839282ce3e40b2c63b70a468e145203c74972a
Prithvi Katkar <prithvirajkatkar17@gmail.com>
2026-03-09 18:25:36 +0530

    added logical operator

---
+++ 02_operators/06_Relational.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a =3;
+    int b= 9;
+    cout<<(a>b)<<endl;
+    cout<<(a<b)<<endl;
+    cout<<(a==b)<<endl;//a=b
+    cout<<(a!=b)<<endl;//a not equal to b
+    return 0;
+}
