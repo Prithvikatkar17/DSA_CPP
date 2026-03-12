adec9bbaacfa627d977a7ea039f0272aed18eb1b
Prithvi Katkar <prithvirajkatkar17@gmail.com>
2026-03-12 22:47:00 +0530

    added conditional example

---
+++ 03_conditional_statement/02_Problem_1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int num ;
+    cout << "Enter the number :";
+    cin >> num ;
+     if (num%2 == 0){
+        cout << "Number is EVEN "<< endl ;
+     }else{
+        cout << "Number is ODD "<< endl;
+     }
+}
